68182aecae1708c4eed1f2eab9b148ecd81bbdd7
CAOSExabyte <55938181+CAOSExabyte@users.noreply.github.com>
2019-10-26 19:49:27 -0700

    Added Main Bot Class

---
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot_Name.java
@@ -0,0 +1,14 @@
+/*
+package org.firstinspires.ftc.teamcode;
+
+// Bot class where we will call other classes to run
+//Rename the class once a bot name is determined
+public class Bot_Name {
+}
+
+Features to add:
+
+Slowdown button, reduces speed by 20,30 or 50 percent, TBD
+Reverse drive direction
+full retraction button
+*/
