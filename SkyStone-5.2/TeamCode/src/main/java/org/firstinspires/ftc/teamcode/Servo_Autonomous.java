11db0076bed0a53d0b84ad9e12ae0ff10b064d30
CAOSExabyte <55938181+CAOSExabyte@users.noreply.github.com>
2019-11-02 16:27:39 -0700

    Added code for web cam, TensorFlow & vueforia

---
+++ SkyStone-5.2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servo_Autonomous.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Servo_Autonomous", group="test")
+public class Servo_Autonomous extends LinearOpMode {
+
+    Servo left_hook, right_hook;
+    /* Declare OpMode members. */
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private DcMotor FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor;
+    HolonomicDrive holonomicDrive;
+
+    @Override
+    public void runOpMode() {
+
+        FrontRightMotor  = hardwareMap.get(DcMotor.class, "front_right_drive");
+        FrontLeftMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
+        BackRightMotor  = hardwareMap.get(DcMotor.class, "back_right_drive");
+        BackLeftMotor = hardwareMap.get(DcMotor.class, "back_left_drive");
+
+        holonomicDrive = new HolonomicDrive(FrontRightMotor, FrontLeftMotor, BackRightMotor, BackLeftMotor);
+
+        left_hook = hardwareMap.servo.get("left_hook");
+        right_hook = hardwareMap.servo.get("right_hook");
+
+        left_hook .setPosition(.5);
+        right_hook.setPosition(0.7);
+
+        waitForStart();
+
+        runtime.reset();
+        holonomicDrive.autoDrive(0, 0.5);
+        while (opModeIsActive() && runtime.seconds() < 1.4){
+            telemetry.addData("Path", "TIME: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        holonomicDrive.stopMoving();
+        runtime.reset();
+        left_hook.setPosition(.9);
+        right_hook.setPosition(.4);
+        while (opModeIsActive() && runtime.seconds() < 1.0) {
+            telemetry.addData("Path", "TIME: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        holonomicDrive.stopMoving();
+        runtime.reset();
+        holonomicDrive.autoDrive(180, 0.5);
+        while (opModeIsActive() && runtime.seconds() < 1.9
+        ){
+            telemetry.addData("Path", "TIME: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        holonomicDrive.stopMoving();
+        runtime.reset();
+        left_hook.setPosition(0.5);
+        right_hook.setPosition(0.7);
+        while (opModeIsActive() && runtime.seconds() < 1.0){
+            telemetry.addData("Path", "TIME: %2.5f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+
+    }
+
+
+}
